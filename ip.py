fa5bf5d26b8519d208dc7caf267fc4db21da7e95
lordLegal <beneder.martin@gmail.com>
2020-11-07 21:06:16 +0100

    second

--- ip.py
+++ ip.py
@@ -4,15 +4,15 @@ import time
 t = time.strftime("%d, %m, %Y")
 geschrieben = False
 while True:
-	h = time.strftime("%H") 
-	m = time.strftime("%M")
-	if (h=="16") and (m=="40") and geschrieben == False:
-		a = requests.get('http://ip.42.pl/raw').text
-		t = time.strftime("%d, %m, %Y")
-		with open("ip.txt", "w") as fh:
-			fh.write(a + "--" + t)
-		print("Feritg")
-		geschriben = True
-	
-	if (h=="00") and (m=="00") and geschrieben == True:
+    h = time.strftime("%H") 
+    m = time.strftime("%M")
+    if (h=="21") and (m=="05") and geschrieben == False:
+        a = requests.get('http://ip.42.pl/raw').text
+        t = time.strftime("%d, %m, %Y")
+        with open("ip.txt", "a") as fh:
+            fh.write(a + "---" + t + "   ")
+        print("Feritg")
+        geschrieben = True
+    
+    if (h=="00") and (m=="00") and (geschrieben == True):
         geschrieben = False
